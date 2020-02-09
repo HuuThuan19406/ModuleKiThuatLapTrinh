444594b2940328301498868159078471fb621e95
Thuận Nguyễn <thuannh19406@st.uel.edu.vn>
2020-02-09 10:01:27 +0700

    Second commit

--- BaiTapNhom/MainWindow.xaml.cs
+++ BaiTapNhom/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace BaiTapNhom
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
