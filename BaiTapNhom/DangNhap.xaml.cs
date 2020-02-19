5f1ee2bb26c03e449bbb34934a09fb69d4f5ec21
HuuThuan19406 <thuannh19406@st.uel.edu.vn>
2020-02-19 20:24:19 +0700

    Tạo class HienThi, ChuyenDoi và SQL trong CLASS

--- BaiTapNhom/DangNhap.xaml.cs
+++ BaiTapNhom/DangNhap.xaml.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,25 +19,31 @@ namespace BaiTapNhom
     /// </summary>
     public partial class DangNhap : Window
     {
+        WaitForLoading frmWait;
         public DangNhap()
         {
             InitializeComponent();
             txtTaiKhoan.Text = "admin";
             pwbMatKhau.Password = "123456";
+            frmWait = new WaitForLoading();
+            //frmWait.Show();
+            frmWait.Hide();
         }
 
         private void DangNhap_Click(object sender, RoutedEventArgs e)
         {
             if ((txtTaiKhoan.Text.ToLower() == "admin") && (pwbMatKhau.Password == "123456"))
             {
+                frmWait.Show();
                 MainWindow frmMain = new MainWindow();
                 frmMain.Show();
+                frmWait.Close();
                 Close();
             }
             else
                 MessageBox.Show("THÔNG TIN MẶC ĐỊNH" +
                                 "\nTài khoản: admin" +
-                                "\nMật khẩu: 123456", "Nhắc nhở",MessageBoxButton.OK,MessageBoxImage.Information);               
+                                "\nMật khẩu: 123456", "Nhắc nhở", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
